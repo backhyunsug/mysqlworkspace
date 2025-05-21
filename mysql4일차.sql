3e7084e551129900777c85bdf6649b7e6b442288
bhs <littleconan@hanmail.net>
2025-05-21 16:44:06 +0900

    0521

--- mysql4일차.sql
+++ mysql4일차.sql
@@ -325,7 +325,59 @@ select stadium_id, sche_date,
 	end 이긴팀 
 from schedule;
 
+-- mysql의 auto_increment mssql의 일련번호 
+-- 오라클은 시퀀스 
+-- mysql에서 auto_increment 속성은 테이블에 하나의 필드만 
+-- 반드시 primary key여야 한다. 
+drop table tb_guestbook;
+create table tb_guestbook 
+( 
+	id bigint auto_increment primary key,
+    title varchar(1000) not null,
+    contents text,
+    writer bigint, -- tb_member와 foreign key로 묶음 
+    regdate datetime 
+);
 
-     
+show tables;
+
+-- 외래키    tb_guestbook 이 tb_member를 참조하는 구조이다
+alter table tb_guestbook 
+add constraint fk_guestbook_member 
+foreign key (writer) 
+references tb_member(member_id);
+
+-- information_schema, mysql, performance_schema 
+-- 시스템 데이터베이스 : 시스템이 운영한다 
+ 
+use information_schema;
+show tables;
+select * from tables;
+
+select table_name from tables where table_schema='sakila';
+
+-- foreign key : key_column_usage 
+select * from key_column_usage where table_schema='mydb2';
+
+use mydb2;
+alter table tb_guestbook 
+drop foreign key fk_guestbook_member;
+
+select * from information_schema.key_column_usage 
+where table_schema='mydb2';
+    
+alter table tb_guestbook 
+add constraint fk_guestbook_member 
+foreign key (writer) 
+references tb_member(member_id);
+
+select * from tb_member;
+
+insert into tb_guestbook (title, writer, contents, regdate)
+values('제목1', 1, '내용1', now());
+
+-- foreign key 에러발생 
+insert into tb_guestbook (title, writer, contents, regdate)
+values('제목2', 2, '내용2', now());
 
 
